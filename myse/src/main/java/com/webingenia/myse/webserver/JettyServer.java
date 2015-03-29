9544335c2e51145a07d1b23b11a0130598c5e0fb
Florent Clairambault <florent@clairambault.fr>
2015-03-30 00:01:40 +0200

    Adding a first servlet test.

--- myse/src/main/java/com/webingenia/myse/webserver/JettyServer.java
+++ myse/src/main/java/com/webingenia/myse/webserver/JettyServer.java
@@ -1,17 +1,30 @@
 package com.webingenia.myse.webserver;
 
+import com.webingenia.myse.webserver.servlets.Index;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
 
 public class JettyServer {
 
 	private static Server server;
+	private static ServletHandler handler;
 
 	public static void start() throws Exception {
 		server = new Server(8080);
+
+		loadHandler();
+
 		server.start();
 	}
 
 	public static void stop() throws Exception {
 		server.stop();
+		server.join();
+	}
+
+	private static void loadHandler() {
+		handler = new ServletHandler();
+		handler.addServletWithMapping(Index.class, "/");
+		server.setHandler(handler);
 	}
 }
---
+++ myse/src/main/java/com/webingenia/myse/webserver/servlets/Index.java
@@ -0,0 +1,18 @@
+package com.webingenia.myse.webserver.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Index extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		try(PrintWriter out = resp.getWriter()) {
+			out.println("Hello !");
+		}
+	}
+}
