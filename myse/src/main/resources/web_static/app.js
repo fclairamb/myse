d1212337049cdada13245c5b9cc6bdc1fd867a52
Florent Clairambault <florent@clairambault.fr>
2015-04-01 01:02:03 +0200

    http://campus.codeschool.com/courses/shaping-up-with-angular-js | 1.5

--- myse/src/main/resources/web_static/app.js
+++ myse/src/main/resources/web_static/app.js
@@ -1,1 +1,26 @@
-var app = angular.module('myapp', []);
+(function () {
+	var app = angular.module('myapp', []);
+
+	app.controller('MyappController', function () {
+		this.products = gems;
+	});
+
+	var gems = [{
+			name: 'Louis',
+			price: 1.30,
+			ok: false,
+			hide: false
+		},
+		{
+			name: 'Charles',
+			price: 40,
+			ok: true
+		},
+		{
+			name: 'Alice',
+			price: 0,
+			ok: true,
+			hide: true
+		}
+	];
+})();
