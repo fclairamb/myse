fdc1c6ab6f9849916f57339b6b02967165f025a2
Florent Clairambault <florent@clairambault.fr>
2015-04-01 03:04:38 +0200

    Progress on tutorial (5.2)

--- myse/src/main/resources/web_static/app.js
+++ myse/src/main/resources/web_static/app.js
@@ -1,5 +1,5 @@
 (function () {
-	var app = angular.module('myapp', []);
+	var app = angular.module('myapp', ['tabs']);
 
 	app.controller('MyappController', function () {
 		this.products = gems;
@@ -37,22 +37,5 @@
 			templateUrl: 'product-top.html'
 		};
 	});
-
-	app.directive('myPanels', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'panels.html',
-			controller: function () {
-				this.tab = 1;
-				this.selectTab = function (tab) {
-					this.tab = tab;
-				};
-				this.isSelected = function (checkTab) {
-					//console.log('isSelected( ' + this.tab + ' / ' + checkTab + ' )');
-					return this.tab === checkTab;
-				};
-			},
-			controllerAs: 'pan'
-		};
-	});
+	
 })();
