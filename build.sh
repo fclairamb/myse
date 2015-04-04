a93e13fe2cf024f6b384e0aaa15bdcf3a4d3f9c0
Florent Clairambault <florent@clairambault.fr>
2015-04-04 23:49:28 +0200

    Testing some build properties changes.

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 ROOT=$(pwd)
 
+BUILD_DATE=$(date +%Y-%m-%d_%H-%M-%S)
+GIT_COMMIT=$(git rev-parse HEAD)
+GIT_COMMIT_COUNT=$(git rev-list HEAD --count)
+
 cd $ROOT/myse
 mvn clean
 mvn assembly:assembly
 
 cd $ROOT
 mkdir -p dist/
+rm dist/* -Rf
 cp myse/target/*dependencies.jar dist/myse_${BUILD_ID}.jar
 
