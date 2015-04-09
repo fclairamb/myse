1ae3f35fd54e628950e61abe3b486b33611013c5
Florent Clairambault <florent@clairambault.fr>
2015-04-09 12:17:22 +0200

    build script improvement.

--- build.sh
+++ build.sh
@@ -2,11 +2,25 @@
 
 set -x
 
+if [ "$release" = "" ]; then
+	echo "No release !"
+	exit 1
+fi
+
 ROOT=$(pwd)
 
 export BUILD_DATE=$(date +%Y-%m-%d_%H-%M-%S)
 export GIT_COMMIT=$(git rev-parse HEAD)
+export GIT_COMMIT_SHORT$(git rev-parse HEAD|head -c5)
 export GIT_COMMIT_COUNT=$(git rev-list HEAD --count)
+
+VERSION=${GIT_COMMIT_COUNT}-${GIT_COMMIT_SHORT}
+
+echo ${VERSION} >dist/version_last
+echo ${VERSION} >version_$release
+cp version_$release dist/version_$release
+
+
 
 cd $ROOT/myse
 mvn clean
@@ -15,5 +29,12 @@ mvn assembly:assembly
 cd $ROOT
 mkdir -p dist/
 rm dist/* -Rf
-cp myse/target/*dependencies.jar dist/myse_${BUILD_ID}.jar
 
+ln myse/target/*dependencies.jar dist/myse_${VERSION}.jar
+sha1sum dist/myse_${VERSION}.jar >dist/myse_${VERSION}.jar.sha1sum
+
+# We add this for people who just want/need to download the jar directly
+if [ "$release" = "stable" ]; then
+	if [ -f dist/myse.jar ]; rm dist/myse.jar; fi
+	ln dist/myse_${VERSION}.jar dist/myse.jar
+fi
