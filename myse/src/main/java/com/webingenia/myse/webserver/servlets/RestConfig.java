a6a4e9801ee0db39b55446d5d91232834e26ea1b
Florent Clairambault <florent@clairambault.fr>
2015-04-13 01:06:09 +0200

    REST: Adding a fonctionnal config manipulation interface.

--- myse/src/main/java/com/webingenia/myse/webserver/servlets/RestConfig.java
+++ myse/src/main/java/com/webingenia/myse/webserver/servlets/RestConfig.java
@@ -1,9 +1,11 @@
 package com.webingenia.myse.webserver.servlets;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.webingenia.myse.access.Source;
 import com.webingenia.myse.db.model.Config;
 import com.webingenia.myse.direxplore.DirExplorer;
+import com.webingenia.myse.webserver.JettyServer;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
@@ -15,17 +17,46 @@ import javax.servlet.http.HttpServletResponse;
 
 public class RestConfig extends HttpServlet {
 
-	private final Gson gson = new Gson();
+	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	public static class ConfigDescription {
+
+		public ConfigDescription(String name, Type type, String description) {
+			this(name, type, description, Config.get(name, null, false));
+		}
+
+		public ConfigDescription(String name, Type type, String description, String value) {
+			this.name = name;
+			this.type = type;
+			this.description = description;
+			this.value = value;
+		}
+
+		public enum Type {
+
+			TEXT,
+			PASSWORD,
+			BOOLEAN
+		}
+		public final String name;
+		public final Type type;
+		public final String description;
+		public final String value;
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String path = req.getPathInfo();
+		if ( path == null ) {
+			path = "";
+		}
 		Object output = null;
 		switch (path) {
 			case "/list":
-				List<Source.PropertyDescription> list = Arrays.asList(
-						new Source.PropertyDescription(Config.PAR_UPDATE_CHANNEL, Source.PropertyDescription.Type.TEXT, "Update channel"),
-						new Source.PropertyDescription(DirExplorer.CONF_LOG_NEW_DIRS, Source.PropertyDescription.Type.BOOLEAN, "Log new dirs")
+				List<ConfigDescription> list = Arrays.asList(
+						new ConfigDescription(Config.PAR_UPDATE_CHANNEL, ConfigDescription.Type.TEXT, "Update channel"),
+						new ConfigDescription(DirExplorer.CONF_LOG_NEW_DIRS, ConfigDescription.Type.BOOLEAN, "Log new dirs"),
+						new ConfigDescription(JettyServer.PROP_PORT, ConfigDescription.Type.TEXT, "Web server port")
 				);
 				output = list;
 				break;
