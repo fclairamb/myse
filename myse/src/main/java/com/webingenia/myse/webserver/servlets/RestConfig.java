1fc3c0d2a0083bb98b56e97d9fedea2d49a38444
Florent Clairambault <florent@clairambault.fr>
2015-04-26 00:35:34 +0200

    Config page: Removing the "software version" setting
    It's only used to perform automatic data (H2 and elasticsearch) upgrade.

--- myse/src/main/java/com/webingenia/myse/webserver/servlets/RestConfig.java
+++ myse/src/main/java/com/webingenia/myse/webserver/servlets/RestConfig.java
@@ -56,8 +56,8 @@ public class RestConfig extends HttpServlet {
 				List<ConfigDescription> list = Arrays.asList(
 						new ConfigDescription(Config.PAR_UPDATE_CHANNEL, ConfigDescription.Type.TEXT, "Update channel"),
 						new ConfigDescription(DirExplorer.CONF_LOG_NEW_DIRS, ConfigDescription.Type.BOOLEAN, "Log new dirs"),
-						new ConfigDescription(JettyServer.PROP_PORT, ConfigDescription.Type.TEXT, "Web server port"),
-						new ConfigDescription(Main.VERSION, ConfigDescription.Type.TEXT, "Software version")
+						new ConfigDescription(JettyServer.PROP_PORT, ConfigDescription.Type.TEXT, "Web server port")
+//						new ConfigDescription(Main.VERSION, ConfigDescription.Type.TEXT, "Software version")
 				);
 				output = list;
 				break;
